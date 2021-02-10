7b3160f4cdef5b13525c3c4a8a8eb3ae4b238d6a
El-houriya <76474641+El-houriya@users.noreply.github.com>
2021-02-10 13:52:07 +0100

    Update jenkTest.php

--- jenkTest.php
+++ jenkTest.php
@@ -1,1 +1,13 @@
-cxx
+<?php
+class Car {
+    function Car() {
+        $this->model = "Tesla";
+    }
+}
+
+// create an object
+$Lightning = new Car();
+
+// show object properties
+echo $Lightning->model;
+?>
